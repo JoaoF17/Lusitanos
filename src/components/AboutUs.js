b1cac1638cf973395a14343e5cda4a47217d5526
JoaoF17 <joao_fernandess17@hotmail.com>
2023-03-09 22:05:31 +0700

    About Us update

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -4,13 +4,13 @@ function AboutUs (){
     return(
         <div className="about-container">
             <h1>Who We Are</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <p>At Lusitanos, our mission is to use the beautiful game of football to help young players develop not only as athletes, but also as individuals. Through a focus on technical skill, tactical awareness, and strong values, we aim to create a positive, inclusive, and supportive environment that helps all our players achieve their full potential.</p>
 
             <h1>Our Mission</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <p>Our vision at Lusitanos is to become a leading football academy, known not only for our success on the pitch, but also for our commitment to developing well-rounded, empathetic, and resilient individuals. Through a focus on technical excellence, teamwork, and a strong sense of community, we aim to create an environment that inspires all our players to strive for greatness, both on and off the field.</p>
 
             <h1>Our Vision</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <p>Lusitanos is more than just a football academy - we are a community of players, coaches, and supporters who share a deep passion for the beautiful game. Our focus is not just on winning games or developing elite athletes, but on helping all our players to become the best versions of themselves, both on and off the field.</p>
         </div>
     );
 }
