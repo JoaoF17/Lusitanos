bbec89b89ace3b3c985e7f48518f588d46e92b40
JoaoF17 <joao_fernandess17@hotmail.com>
2023-03-10 14:46:52 +0700

    Typo

--- src/routes/About.js
+++ src/routes/About.js
@@ -9,7 +9,7 @@ function About (){
         <Hero
         cName="hero-mid"
         heroImg="https://images.unsplash.com/photo-1434648957308-5e6a859697e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
-        title="Abous Lusitanos"
+        title="About Lusitanos"
         text=""
         buttonText="Find out more."
         url="/"
