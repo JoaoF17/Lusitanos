6e488ef43c292574eb0a56e9b6e21f6854958345
JoaoF17 <joao_fernandess17@hotmail.com>
2023-04-14 23:28:01 +0700

    photo update

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -2,6 +2,7 @@ import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import ContactUs from "../components/Contact/ContactUs";
 import Footer from "../components/Footer";
+import getintouch from "../Assets/getintouch.jpg"
 
 function Contact (){
     return(
@@ -9,7 +10,7 @@ function Contact (){
         <Navbar />
         <Hero
         cName="hero-mid"
-        heroImg="https://images.unsplash.com/photo-1601176682258-172d20729a0c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1309&q=80"
+        heroImg={getintouch}
         title="Get in touch"
         text="We are here to help young athletes become the stars of the future."
         buttonText="Find out more."
