2c8dbab64af70ee1019cb049400f2cdfac1ec2b0
JoaoF17 <joao_fernandess17@hotmail.com>
2023-04-14 23:22:07 +0700

    pictures update

--- src/routes/About.js
+++ src/routes/About.js
@@ -2,6 +2,7 @@ import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import AboutUs from "../components/About/AboutUs";
 import Footer from "../components/Footer";
+import aboutpic from "../Assets/about.jpg"
 
 function About (){
     return(
@@ -9,7 +10,7 @@ function About (){
         <Navbar />
         <Hero
         cName="hero-mid"
-        heroImg="https://images.unsplash.com/photo-1434648957308-5e6a859697e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
+        heroImg= {aboutpic}
         title="About Lusitanos"
         text=""
         buttonText="Find out more."
--- src/routes/Coaches.js
+++ src/routes/Coaches.js
@@ -2,6 +2,7 @@ import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import OurCoaches from "../components/Our Coaches/OurCoaches";
 import Footer from "../components/Footer";
+import coachespic from "../Assets/ourcoaches.jpg"
 
 import React from 'react'
 
@@ -11,7 +12,7 @@ function Coaches() {
             <Navbar />
             <Hero
                 cName="hero-mid"
-                heroImg="https://images.unsplash.com/photo-1508098682722-e99c43a406b2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                heroImg={coachespic}
                 title="Meet our coaches"
                 text=""
                 url="/"
