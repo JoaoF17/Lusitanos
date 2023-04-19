7bba0b454f9d08e25f88862a0bc6884b0386df48
JoaoF17 <joao_fernandess17@hotmail.com>
2023-04-19 20:30:08 +0700

    ambassadors page update

--- src/components/Ambassadors/OurAmbassadors.js
+++ src/components/Ambassadors/OurAmbassadors.js
@@ -13,7 +13,7 @@ function OurAmbassadors() {
         <div className="ambassadors">
 
             <h2>Meet our Ambassadors</h2>
-            <h4>We are proud to have the support of some of the most talented and inspiring football players in the game today.These players share our passion for football and our commitment to developing the next generation of stars.
+            <h4>We are proud to have the support of some of the most talented and inspiring football players in the game today.These players share our passion for football and our commitment to developing the next generation of stars. The endorsement of our training methodology by our ambassadors, who have a wealth of football experience, speaks to its effectiveness.
                 <br /> 
                 Our ambassadors are more than just famous faces – they are role models for young players everywhere. Through their hard work, dedication, and exceptional skills, they have achieved great success on and off the pitch. They know what it takes to become a great football player, and they are eager to share their knowledge and experience with our students.
             </h4>
@@ -45,7 +45,7 @@ function OurAmbassadors() {
             <AmbassadorsData
                 className="ambassador"
                 ambassadorheading="HEBERTY FERNANDES | BANGKOK UNITED"
-                ambassadordescription="Heberty Fernandes de Andrade is a Brazilian footballer who plays for Thai League 1 club Bangkok United."
+                ambassadordescription="Heberty Fernandes de Andrade is a Brazilian footballer who plays for Thai League 1 club Bangkok United and currently holds the record for most goals scored in the League, EVER."
                 img={Heberty}
             />
             <AmbassadorsData
