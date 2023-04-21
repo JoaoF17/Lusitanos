2506d03a0680b0c4715519ed0a099fa784dc5aa9
JoaoF17 <joao_fernandess17@hotmail.com>
2023-04-21 12:29:06 +0700

    about update

--- src/components/About/AboutUs.js
+++ src/components/About/AboutUs.js
@@ -4,13 +4,14 @@ function AboutUs (){
     return(
         <div className="about-container">
             <h1>Who We Are</h1>
-            <p>At Lusitanos, our mission is to use the beautiful game of football to help young players develop not only as athletes, but also as individuals. Through a focus on technical skill, tactical awareness, and strong values, we aim to create a positive, inclusive, and supportive environment that helps all our players achieve their full potential.</p>
+            <p>Lusitanos is more than just a football academy - we are a community of players, coaches, and supporters who share a deep passion for the beautiful game. Our focus is not just on winning games or developing elite athletes, but on helping all our players to become the best versions of themselves, both on and off the field.</p>
 
             <h1>Our Mission</h1>
-            <p>Our vision at Lusitanos is to become a leading football academy, known not only for our success on the pitch, but also for our commitment to developing well-rounded, empathetic, and resilient individuals. Through a focus on technical excellence, teamwork, and a strong sense of community, we aim to create an environment that inspires all our players to strive for greatness, both on and off the field.</p>
-
+            <p>At Lusitanos, our mission is to use the beautiful game of football to help young players develop not only as athletes, but also as individuals. Through a focus on technical skill, tactical awareness, and strong values, we aim to create a positive, inclusive, and supportive environment that helps all our players achieve their full potential.</p>
+            
             <h1>Our Vision</h1>
-            <p>Lusitanos is more than just a football academy - we are a community of players, coaches, and supporters who share a deep passion for the beautiful game. Our focus is not just on winning games or developing elite athletes, but on helping all our players to become the best versions of themselves, both on and off the field.</p>
+            <p>Our vision at Lusitanos is to become a leading football academy, known not only for our success on the pitch, but also for our commitment to developing well-rounded, empathetic, and resilient individuals. Through a focus on technical excellence, teamwork, and a strong sense of community, we aim to create an environment that inspires all our players to strive for greatness, both on and off the field.</p>
+            
         </div>
     );
 }
